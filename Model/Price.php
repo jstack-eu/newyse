b512c09f003e0f28cb4bb38dab98d48dae869737
Stefan van den Broek <stefan.vandenbroek@jstack.eu>
2019-06-26 11:51:20 +0200

    Add property referencePrice to model price

--- Model/Price.php
+++ Model/Price.php
@@ -25,6 +25,11 @@ class Price
     protected $referencePriceInclusive;
 
     /**
+     * @var float
+     */
+    protected $referencePrice;
+
+    /**
      * @var \DateTime
      */
     protected $arrivalDate;
@@ -101,6 +106,32 @@ class Price
     {
         $this->referencePriceInclusive = $referencePriceInclusive;
         return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getReferencePriceInclusive()
+    {
+        return $this->referencePriceInclusive;
+    }
+
+    /**
+     * @param float $referencePrice
+     * @return Price
+     */
+    public function setReferencePrice($referencePrice)
+    {
+        $this->referencePrice = $referencePrice;
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getReferencePrice()
+    {
+        return $this->referencePrice;
     }
 
     /**
