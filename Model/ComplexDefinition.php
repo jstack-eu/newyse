300cdce672e54ef011e67cbfbacce349fabe335c
Rick van Laarhoven <hello@rickvanlaarhoven.nl>
2019-09-11 22:55:07 +0200

    Added some tests

--- Model/ComplexDefinition.php
+++ Model/ComplexDefinition.php
@@ -9,13 +9,49 @@ namespace Jstack\Newyse\Model;
  */
 class ComplexDefinition
 {
-    /**
-     * @var integer
-     */
+    /** @var integer */
     protected $complexDefinitionId;
 
-    /**
-     * @var string
-     */
+    /** @var string */
     protected $code;
+
+    /**
+     * @return int
+     */
+    public function getComplexDefinitionId()
+    {
+        return $this->complexDefinitionId;
+    }
+
+    /**
+     * @param int $complexDefinitionId
+     *
+     * @return ComplexDefinition
+     */
+    public function setComplexDefinitionId($complexDefinitionId)
+    {
+        $this->complexDefinitionId = $complexDefinitionId;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param string $code
+     *
+     * @return ComplexDefinition
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
 }
